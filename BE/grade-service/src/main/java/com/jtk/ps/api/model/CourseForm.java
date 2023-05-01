4fe827c69e8d649fb3a5020ffc7857e90fe5ff0e
kusnadinali <muhammad.ali.tif20@polban.ac.id>
2023-05-01 09:52:49 +0700

    menambah model dan repository courseform

---
+++ BE/grade-service/src/main/java/com/jtk/ps/api/model/CourseForm.java
@@ -0,0 +1,38 @@
+package com.jtk.ps.api.model;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "course_form")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseForm {
+    
+    @Id
+    @Column(unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "is_deleted")
+    private Integer isDeleted;
+
+    @Column(name = "prodi_id", nullable = false)
+    private Integer prodiId;
+
+    @Column(name = "kode",nullable = false)
+    private String kode;
+
+    @Column(name = "tahun_ajaran_start")
+    private Integer tahunAjaranStart;
+
+    @Column(name = "tahun_ajaran_end")
+    private Integer tahunAjaranEnd;
+
+    @Column(name = "sks")
+    private Integer sks;
+}
