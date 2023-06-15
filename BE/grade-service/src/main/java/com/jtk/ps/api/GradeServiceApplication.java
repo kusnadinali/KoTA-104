12dcff836f3da5c5f2905a4f8a84c109e32cb6ef
kusnadinali <muhammad.ali.tif20@polban.ac.id>
2023-06-15 14:15:37 +0700

    try integration

--- BE/grade-service/src/main/java/com/jtk/ps/api/GradeServiceApplication.java
+++ BE/grade-service/src/main/java/com/jtk/ps/api/GradeServiceApplication.java
@@ -2,18 +2,29 @@ package com.jtk.ps.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GradeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradeServiceApplication.class, args);
 	}
 
-	@Bean
+    @Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+    
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
